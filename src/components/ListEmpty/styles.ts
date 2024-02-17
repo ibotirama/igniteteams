9bffa2d53064fd74adc33cf5876e4c8ef30d61bb
ibotirama <pedro.henrique1@gmail.com>
2024-02-17 12:50:16 -0300

    Lista de turmas

---
+++ src/components/ListEmpty/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Message = styled.Text`
+    font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+
+    text-align: center;
+    margin-top: 48px;
+`;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,35 @@
 import { Header } from '@components/Header';
-import { Container, Title } from './styles';
+import { Container } from './styles';
+import { HighLight } from '@components/HighLight/index.';
+import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera do ignite.', 'Galera do mobile.']);
+
   return (
     <Container>
       <Header />
+      <HighLight
+        title='Turmas'
+        subtitle='Jogue com a sua turma'
+      />
+
+      {groups.length === 0 ?
+        <ListEmpty message='Que tal cadastrar a primeira turma?' />
+        :
+        <FlatList
+          data={groups}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <GroupCard
+              title={item}
+            />
+          )}
+        />
+      }
     </Container>
   );
 }
