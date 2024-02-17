fe6d2dd6e9d10169de6a789aa84eb852d5867b1e
ibotirama <pedro.henrique1@gmail.com>
2024-02-17 13:53:13 -0300

    Botao customizado adicionado na tela principal

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera do ignite.', 'Galera do mobile.']);
@@ -17,9 +18,6 @@ export function Groups() {
         subtitle='Jogue com a sua turma'
       />
 
-      {groups.length === 0 ?
-        <ListEmpty message='Que tal cadastrar a primeira turma?' />
-        :
         <FlatList
           data={groups}
           keyExtractor={item => item}
@@ -28,8 +26,11 @@ export function Groups() {
               title={item}
             />
           )}
+          contentContainerStyle={groups.length === 0 && { flex: 1 }}
+          ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
         />
-      }
+
+        <Button title='Criar nova turma' />
     </Container>
   );
 }
