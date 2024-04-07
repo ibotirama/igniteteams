98233a0d50c451e2c15c0ae31848d7b5e7cbb1f7
Pedro Andrade <pedro.henrique1@gmail.com>
2024-04-07 16:18:40 -0300

    feat: reactive native navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "react-native";
-import { Loading } from "@components/Loading";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
-
 import { ThemeProvider } from 'styled-components';
+
+import { Loading } from "@components/Loading";
+import { Routes } from "./src/routes";
+
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
       />
 
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,25 @@
-import { Header } from '@components/Header';
-import { Container } from './styles';
-import { HighLight } from '@components/HighLight/index.';
-import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { Container } from './styles';
+
+import { useNavigation } from '@react-navigation/native'
+
+import { HighLight } from '@components/HighLight/index.';
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+
+
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera do ignite.', 'Galera do mobile.']);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup(): void {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -18,19 +29,22 @@ export function Groups() {
         subtitle='Jogue com a sua turma'
       />
 
-        <FlatList
-          data={groups}
-          keyExtractor={item => item}
-          renderItem={({ item }) => (
-            <GroupCard
-              title={item}
-            />
-          )}
-          contentContainerStyle={groups.length === 0 && { flex: 1 }}
-          ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
-        />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
+      />
 
-        <Button title='Criar nova turma' />
+      <Button
+        title='Criar nova turma'
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
